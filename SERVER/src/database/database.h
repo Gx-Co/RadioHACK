d7cd10cd4843318db5171321f38248888ff8273c
ModestV <vladpolovinka@mail.ru>
2025-10-08 01:58:33 +0500

    Update database.h

--- SERVER/src/database/database.h
+++ SERVER/src/database/database.h
@@ -1,1 +1,10 @@
-SERVER
+#pragma once
+#include <string>
+using namespace std;
+
+// функции для работы с user.json
+void CUDB(const string& login, const string& password, const string& role, const string& level);
+void CDDB(const string& login, const string& password="", const string& role="", const string& level="");
+void DUDB(const string& login);
+int SUDB(const string& login, const string& password);
+string SLDB(const string& login);
